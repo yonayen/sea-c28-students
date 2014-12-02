2024b31409edb2c8c70b5f936203b185ae687768
Yonathan Ayenew <yonayen@gmail.com>
2014-12-01 17:18:59 -0800

    added assert statemetns after if __name == '___main__': block

--- Students/YonathanAyenew/series.py
+++ Students/YonathanAyenew/series.py
@@ -3,16 +3,16 @@
 # Fibonacci Series
 
 def fibonacci(n):
-    """Using looping technique, start with integers 0 and 1."""
-    a,b = 0, 1
+    """ Using looping technique, start with integers 0 and 1."""
+    a, b = 0, 1
     for i in range (n-1):
-   	    a,b = b, a + b
+   	    a, b = b, a + b
     return a
 
 # Lucas Numbers
 
 def lucas(n):
-	""" Using looping technique, start with integers 2 and 1 """
+	""" Using looping technique, start with integers 2 and 1. """
     a, b = 2, 1
     for i in range (n-1):
         a, b = b, a + b
@@ -21,8 +21,20 @@ def lucas(n):
 # Sum_Series function
 
 def sum_series(n):
-    """ Start with any two integers to begin looping technique """
+    """ Start with any two integers to begin looping technique. """
     a, b = 0, 1
     for i in range (n-1):
         a, b = b, a + b
     return a	
+
+
+if __name__ == '__main__':
+    if a, b = 0, 1:
+        assert "This is a Fibonacci Series"
+
+    elif a, b = 2, 1:
+        assert "This is a Lucas Series"
+
+    else:
+        assert "This is a Sum Series"
+  
