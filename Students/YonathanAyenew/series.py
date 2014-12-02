45c9c3b613449f81ada3f99ef3a474d53f4209db
Yonathan Ayenew <yonayen@gmail.com>
2014-12-01 17:02:48 -0800

    added Lucas number with looping technique

--- Students/YonathanAyenew/series.py
+++ Students/YonathanAyenew/series.py
@@ -8,3 +8,12 @@ def fibonacci(n):
     for i in range (n-1):
    	    a,b = b, a + b
     return a
+
+# Lucas Numbers
+
+def lucas(n):
+	""" Using looping technique, start with integers 2 and 1 """
+    a, b = 2, 1
+    for i in range (n-1):
+        a, b = b, a + b
+    return a
