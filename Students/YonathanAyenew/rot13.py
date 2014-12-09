d46835ecbe5678b0a1ab5574b63c3421cd73618b
Yonathan Ayenew <yonayen@gmail.com>
2014-12-08 18:00:30 -0800

    started working on the module test.  Not good enough yet.

--- Students/YonathanAyenew/rot13.py
+++ Students/YonathanAyenew/rot13.py
@@ -8,4 +8,28 @@ rot13 = string.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
 
 string.translate("Yonathan Ayenew!", rot13)
 
+
 if __name__ == '__main__':
+
+dictionary = {'a':'n', 'b':'o', 'c':'p','d':'q', 'e':'r', 'f':'s',
+             'g':'t','h':'u','i':'v','j':'w', 'k':'x','l':'y',
+             'm':'z','n':'a','o':'b','p':'c','q':'d','r':'e',
+             's':'f','t':'g','u':'h','v':'i', 'w':'j','x':'k','y':'l','z':'m'}	
+
+def rot(xy):
+	rot13 = ''
+	for letter in xy:
+		if letter.islower():
+			rot13 =+ dictionary.get(letter)
+		else:
+			print 'now working'
+
+	for letter in xy:
+		if letter.isupper():
+			letter = letter.lower()
+			rot13 =+ dictionary.get(letter).capitalize()
+		else:
+			print 'not working'
+
+	# This obviously needs a lot of work.  I'll revisit it again. I'm over thinking a simple test.
+
