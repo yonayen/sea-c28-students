c5ef0cad64ffeea1f84a3af3380b5209d1610e22
Yonathan Ayenew <yonayen@gmail.com>
2014-12-13 18:19:15 -0800

    corrections to Ackerman function with test module

--- Students/YonathanAyenew/ack.py
+++ Students/YonathanAyenew/ack.py
@@ -1,44 +1,37 @@
+#!/usr/bin/env python
 
+#Ackerman Function
 
 def ack (m, n):
-    """For ack -- values m and n are non-negative integers."""
-	if m == 0:
-		return (n + 1)
-	elif m > 0 and n == 0:
-		return ack(m - 1, 1)
-	else:
-		return ack(m - 1, ack(m, n - 1))
+    """ Solve for Ackerman fucntion (values m and n are non-negative integers)."""
+    if m < 0 or n < 0:
+        return None
+    if m == 0:
+        return (n + 1)
+    elif n == 0:
+        return ack(m - 1, 1)
+    else:
+        return ack(m - 1, ack(m, n - 1))
 
 
 if __name__ == '__main__':
 
-	print ack (0, 0)
+    assert ack(0, 0) == 1
+    assert ack(0, 4) == 5
+  
+    assert ack(1, 2) == 4
+    assert ack(1, 4) == 6
+ 
+    assert ack(2, 2) == 7
+    assert ack(2, 4) == 11
+    
+    assert ack(3, 2) == 29
+    assert ack(3, 4) == 125
+    
+    assert ack(-1, 0) is None
+    assert ack(2, -1) is None
+    
+    # When m > 3, RuntimeError: maximum recursion depth exceeded 
+    assert ack(4, 1)
 
-	print ack (0, 1)
-	print ack (0, 2)
-	print ack (0, 3)
-	print ack (0, 4)
-
-	print ack (1, 0)
-	print ack (1, 1)
-	print ack (1, 2)
-	print ack (1, 3)
-	print ack (1, 4)
-
-	print ack (2, 0)
-	print ack (2, 1)
-	print ack (2, 2)
-	print ack (2, 3)
-	print ack (2, 4)
-
-	print ack (3, 0)
-	print ack (3, 1)
-	print ack (3, 2)
-	print ack (3, 3)
-	print ack (3, 4)
-
-	# "All tests pass"
-
-	print ack (4, 1)
-	# When m > 3, RuntimeError: maximum recursion depth exceeded 
-	#
+    print u"all the tests passed"
