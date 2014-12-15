974db74c3561bb62c6844eba4fba75163a063e83
Yonathan Ayenew <yonayen@gmail.com>
2014-12-15 00:43:52 -0800

    latest changes to rot13.py including a test that works

--- Students/YonathanAyenew/rot13.py
+++ Students/YonathanAyenew/rot13.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Compute rot13 -- replace letter with one 13 places further in alphabet"""
+
+dictionary = {'a':'n', 'b':'o', 'c':'p','d':'q', 'e':'r', 'f':'s','g':'t','h':'u','i':'v','j':'w', 'k':'x','l':'y',
+             'm':'z','n':'a','o':'b','p':'c','q':'d','r':'e','s':'f','t':'g','u':'h','v':'i', 'w':'j','x':'k','y':'l','z':'m'}
 
 
-import string
+def rot(word):
+    rot = []
+    for c in word:
+        if c.islower():
+            rot.append(dictionary.get(c))
+        if c.isupper():
+            c = c.lower()
+            result = dictionary.get(c)
+            rot.append(result.capitalize())
+        if c not in dictionary:
+            rot.append(c)
+ 
+    return ''.join(rot)
 
-rot13 = string.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
-	"NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
-
-string.translate("Yonathan Ayenew!", rot13)
 
 
 if __name__ == '__main__':
 
-dictionary = {'a':'n', 'b':'o', 'c':'p','d':'q', 'e':'r', 'f':'s',
-             'g':'t','h':'u','i':'v','j':'w', 'k':'x','l':'y',
-             'm':'z','n':'a','o':'b','p':'c','q':'d','r':'e',
-             's':'f','t':'g','u':'h','v':'i', 'w':'j','x':'k','y':'l','z':'m'}	
+    dictionary = {'a':'n', 'b':'o', 'c':'p','d':'q', 'e':'r', 'f':'s', 'g':'t','h':'u','i':'v','j':'w', 'k':'x','l':'y',
+             'm':'z','n':'a','o':'b','p':'c','q':'d','r':'e', 's':'f','t':'g','u':'h','v':'i', 'w':'j','x':'k','y':'l','z':'m'}  
+    
+    input = "Yonathan Ayenew"
+    rot_reverse = "Lbanguna Nlrarj"
 
-def rot(xy):
-	rot13 = ''
-	for letter in xy:
-		if letter.islower():
-			rot13 =+ dictionary.get(letter)
-		else:
-			print 'now working'
-
-	for letter in xy:
-		if letter.isupper():
-			letter = letter.lower()
-			rot13 =+ dictionary.get(letter).capitalize()
-		else:
-			print 'not working'
-
-	# This obviously needs a lot of work.  I'll revisit it again. I'm over thinking a simple test.
-
+    assert rot("Yonathan Ayenew") == rot_reverse
+    
+    print "Assert test works"
