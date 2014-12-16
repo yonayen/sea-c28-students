30e1afd36f72e0d2fef48b2158851f8ea29da52b
Yonathan Ayenew <yonayen@gmail.com>
2014-12-15 17:41:46 -0800

    dict_set.py latest corrections and imporoved code

--- Students/YonathanAyenew/dict_set.py
+++ Students/YonathanAyenew/dict_set.py
@@ -1,88 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 
 # Dict/Set Lab
+# 
 
-newd = {u'name': u'Chris', u'city': u'Seattle', u'cake': u'Chocolate'}
+newd = {'name': 'Chris', 'city': 'Seattle', 'cake': 'chocolate'}
 print newd
 
-newd.pop(u'cake')
+newd.pop('cake')
 print newd
 
-newd[u'fruit'] = u'mango'
+newd['fruit'] = 'mango'
 print newd
 
-for key in newd.iterkeys():
-    print key
+#  print newd keys
+print newd.keys()
 
-for val in d.itervalues():
-	print val
+# print newd values
+print newd.values()
 
-newd.has_key(u'cake')
-u'mango' in newd.values()
+# this should return false
+print 'cake' in newd.keys()
 
-l1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] 
-l2 = [0, 1, 2, 3, 4, 5, 6 ,7, 8 ,9 ,'a', 'b', 'c', 'd', 'e', 'f']
-new_list = dict(zip(l1, l2))
-print new_list
+print 'mango' in newd.values()
+
+# dict constructor and zip to build range(16) and it's hexidecimal pair
+
+numbers = range(16)
+hexidecimal = []
+for number in numbers:
+    hexidecimal.append(hex(number))
+
+print hexidecimal
+
+# put both numbers and hexidecimals into dict
+hexidecimal_d = dict(zip(numbers, hexidecial))
+
+print hexidecimal_d
+
+# create new dict using same key from begining of this exercise but with number of 'a's in each value 
+
+newd = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+
+newddict = {}
+for key, val in newd.items():
+    newddict[key] = val.count ('a')
+
+print newddict
+
+# changing the values
+
+for key, val in newd.items():
+    newd[key] = val.count('a')
+
+print newd
+
+# 
+# sets s2, s3 and s4 that range(21) -- divisible by 2, 3 and 4
+# 
 
 
-newd = {u'name': u'Chris', u'city': u'Seattle', u'cake': u'Chocolate'}
-# create new dict using same key but with number of 'a's in each value
+s2 = set()
+s3 = set()
+s4 = set() 
 
-for val in newd.itervalues():
-	cal = val.count('a')
-    newd = {k: cal for k in newd}
+for i in range(21):
+    if not i % 2:
+        s2.add(i)
+    if not i % 3:
+        s3.add(i)
+    if not i % 4:
+        s4.add(i)
 
-#  This isn't looping well and applies the same val.count to all keys.  The values all become 0.
+print s2
+print s3
+print s4
 
-s2 = ([2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
-s3 = ([3, 6, 9, 12, 15, 18])
-s4 = ([4, 8, 12, 16, 20])
-
-s4.issubset(s2) 
-# true
-# or s2 >= s4
+# s3 as subset of s2
 
 s3.issubset(s2)
 # false
 
-# This attempt at creating sets wasn't working out
+# s4 as subset of s2
+s4.issubset(s2) 
+# true
+# or s2 >= s4
 
+# 
+# add 'i' to set with letters in 'Python'
+# 
 
-s2 = ()
-for i in range(21):
-	if i % 2 == 0:
-    s2.update(i)
-#  this is one way of creating s2
+s = set(u'python')
+s.add('i')
 
-s3 =()
-for i in range(21):
-	if i % 3 == 0:
-    s3.update(i)
-#  this is one way of creating s3
-
-s4 = ()
-for i in range(21):
-	if i % 4 == 0:
-    s4.update(i)
-# this is one way of creating s4
-
-word = ['Python']
-result = set()
-print set(','.join (word).replace(',', ''))
-
-# print 'python' as indivdual letters in set
-
-my_set = {'p', 'y', 't', 'h','o', 'n'}
-my_set |= {'i'}
-print my_set
-
-# adding 'i' to python set
+print s 
 
 # create  frozenset with letters from 'marathon'
+word = frozenset(u'marathon')
 
-marathon = frozenset(('m','a','r','a','t','h', 'o', 'n'))
+# union & intersection of sets
 
-word.issubset(marathon)
-# return False
+print "union:", s.union(word)
+print "intersection:", s.intersection(word)
+
+print "union:", word.union(s)
+print "intersection:", word.union(s)
 
 
 
