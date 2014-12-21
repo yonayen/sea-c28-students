3547b87d9901ba3356f881483b65742e6ba6d37b
Yonathan Ayenew <yonayen@gmail.com>
2014-12-21 05:34:27 -0800

    added if __name__ = '__main__': block

--- Students/YonathanAyenew/functional_files.py
+++ Students/YonathanAyenew/functional_files.py
@@ -28,3 +28,30 @@ def comprehension_clean(input_name, output_name):
 
     io.open(output_name, 'w').writelines(outlines)
 
+if __name__ == '__main__':
+    
+    if len(sys.argv) < 2:
+        print u'clean the input_name -- clean whitespace in front and back.  Give output_name file to prenvent input_name from being overwritten"
+    
+        sys.exit(1)
+
+    else:
+        input_name = sys.argv[1]
+        try: 
+            output_name = sys.argv[2]
+        except IndexError:
+            input_name = output_name
+
+    map_clean(input_name, output_name)
+    comprehension_clean(input_name, output_name)      
+
+
+
+
+
+
+
+
+
+
+
