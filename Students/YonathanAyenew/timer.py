f6bf802fc9dbc5ec284ef9ae69b96ae07d5ac5a2
Yonathan Ayenew <yonayen@gmail.com>
2014-12-21 16:01:32 -0800

    final edit

--- Students/YonathanAyenew/timer.py
+++ Students/YonathanAyenew/timer.py
@@ -4,10 +4,15 @@ from __future__ import print_function
 import time
 
 class Timer(object):
+    def __init__(self, f=sys.stdout):
+        self.f =f
+
     def __enter__ (self):
         self.start = time.time()
         return self
 
     def __exit__(self, *args):
         self.elapsed = time.time() - self.start
-        print("This code took {} seconds to run.".format(self.elapsed))
+        self.f.write("{} seconds elapsed. \n".format(self.elapsed)) 
+
+
